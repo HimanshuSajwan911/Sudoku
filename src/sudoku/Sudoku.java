2a506e038b32c527a6e34b595314ffedc4bd2951
Himanshu Sajwan <57579061+HimanshuSajwan911@users.noreply.github.com>
2022-03-09 20:12:54 +0530

    Sudoku.java
    Starts MainMenu

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,29 @@
+/*
+
+    Project: Sudoku(6x6) Game.
+
+    Name: Himanshu Sajwan
+    Admission No: 2014402
+    Enrollment No: GE-192014402
+    Course: B.Tech C.S.E.
+
+    Section: E
+    Roll No: 30
+
+    Starting date: 22-October-2020.
+    Compeletion date: 28-October-2020.
+
+*/
+package sudoku;
+
+
+// This class is starting point for the game.
+public class Sudoku {
+
+    public static void main(String[] args) {
+       
+        MainMenu mainmenu = new MainMenu();       
+        mainmenu.setVisible(true);
+    
+    }
+}
